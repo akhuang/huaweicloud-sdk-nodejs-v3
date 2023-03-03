395efcdb89f315205e22dcffa78cb5b45c35bc39
Phoenix <fy24h.huang@gmail.com>
2023-03-03 18:56:42 +0800

    add frs sample

--- samples/frs/CompareFaceByFile.ts
+++ samples/frs/CompareFaceByFile.ts
@@ -1,5 +1,14 @@
-import { FrsClient, CompareFaceByFileRequest, CompareFaceByFileRequestBody, FrsRegion } from "./frs/huaweicloud-sdk-frs";
-import { BasicCredentials } from "../core/auth/BasicCredentials";
+/**
+ * Install dependencies: 
+ * npm install @huaweicloud/huaweicloud-sdk-frs dotenv
+ * 
+ * The sample uses dotenv to manage credential information. 
+ * Before use, please rename .env.sample to .env and input your credential information.
+ * 
+ */
+
+import { FrsClient, CompareFaceByFileRequest, CompareFaceByFileRequestBody, FrsRegion } from "@huaweicloud/huaweicloud-sdk-frs";
+import { BasicCredentials } from "@huaweicloud/huaweicloud-sdk-core/auth/BasicCredentials";
 import * as dotenv from 'dotenv';
 import fs from 'fs';
 import path from 'path';
@@ -7,9 +16,9 @@ import path from 'path';
 dotenv.config();
 
 const ak = process.env.AK;
-const sk = process.env.SK; 
+const sk = process.env.SK;
 
-let credentials = new BasicCredentials()
+const credentials = new BasicCredentials()
     .withAk(ak)
     .withSk(sk);
 
@@ -18,7 +27,7 @@ let credentials = new BasicCredentials()
 })();
 
 async function fetchDataWithRetry() {
-    try { 
+    try {
         const client = FrsClient.newBuilder()
             .withCredential(credentials)
             .withRegion(FrsRegion.CN_EAST_3)
@@ -27,35 +36,12 @@ async function fetchDataWithRetry() {
 
         const body: CompareFaceByFileRequestBody = new CompareFaceByFileRequestBody();
 
-        const filePath = path.join(__dirname, 'frs/images/1.jpeg');
+        const filePath = path.join(__dirname, 'samples/images/1.jpeg');
         body.withImage1File(fs.createReadStream(filePath))
         body.withImage2File(fs.createReadStream(filePath))
         request.withBody(body)
         const result = await client.compareFaceByFile(request);
         console.log("Result:", JSON.stringify(result, null, 2));
-
-        /*
-        {
-            "image1_face": {
-              "bounding_box": {
-                "width": 261,
-                "top_left_x": 170,
-                "top_left_y": 90,
-                "height": 364
-              }
-            },
-            "similarity": 1,
-            "image2_face": {
-              "bounding_box": {
-                "width": 261,
-                "top_left_x": 170,
-                "top_left_y": 90,
-                "height": 364
-              }
-            },
-            "httpStatusCode": 200
-        }
-        */
     } catch (error: any) {
         console.error("Exception:", JSON.stringify(error, null, 2));
     }
